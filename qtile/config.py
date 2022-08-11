a1944b941db61637cf6c40bbeb9db841a00a5804
Manuel-Camacho-Padilla <manu320camacho@gmail.com>
2022-08-11 01:34:57 -0500

    0.14

--- qtile/config.py
+++ qtile/config.py
@@ -4,7 +4,8 @@ from modules.layouts import layouts, floating_layout
 from modules.mouse import mouse
 from modules.hooks import *
 import os
-from modules.screens import screens
+from modules.screens import screens, widget, widget_defaults
+
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
 main = None  # WARNING: this is deprecated and will be removed soon
@@ -15,7 +16,8 @@ auto_fullscreen = True
 focus_on_window_activation = "smart"
 wmname = "Qtile"
 widget_defaults = dict(
-        font='Cascadia Code',
-        fontsize=13,
-        padding=3
+    font="JetBrainsMono Nerd Font",
+    fontsize=16,
+    padding=3,
+    foreground=catppuccin["black1"],
 )
