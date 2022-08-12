294cd60d747a4455578750f931ffca18610d74fc
Manuel-Camacho-Padilla <manu320camacho@gmail.com>
2022-08-12 08:33:45 -0500

    0.18

--- qtile/autostart.sh
+++ qtile/autostart.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 picom &  disown # --experimental-backends --vsync should prevent screen tearing on most setups if needed
-setxkbmap latam & disown
-dunst & disown
+setxkbmap latam & disown # Layout keyboard LATAM
+dunst & disown 
 blueman-applet & disown
 nm-applet & disown
 xfce4-clipman & disown
 xclip & disown
+
+
+ln $HOME/.config/qtile/config/starship.toml $STARSHIP
+
